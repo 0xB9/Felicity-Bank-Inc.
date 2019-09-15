84885f54275411f629b2be9a263ffe83cf3bf368
0xB9 <0xB9@pm.me>
2019-09-15 03:26:24 -0400

    Add JS

---
+++ assets/js/account.js
@@ -0,0 +1,17 @@
+function getDate(num) {
+  var date = new Date();
+  date.setDate(date.getDate() - num);
+  return date.getMonth() + 1 + "/" + date.getDate();
+}
+
+function replaceTextInClass(className, text) {
+  var elms = document.querySelectorAll("." + className),
+    i;
+  for (i = 0; i < elms.length; ++i) {
+    elms[i].textContent = text;
+  }
+}
+
+replaceTextInClass("today", getDate(0));
+replaceTextInClass("yesterday", getDate(1));
+replaceTextInClass("twodays", getDate(2));
